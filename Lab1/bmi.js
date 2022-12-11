af398d274b6fff6fa0183da926b274cb42e8a4b1
Siobhán O'Hanlon <80902477+siobhanohanlon@users.noreply.github.com>
2022-12-11 13:38:57 +0000

    Lab1- Classes in JavaScript

---
+++ Lab1/bmi.js
@@ -0,0 +1,38 @@
+//New Class made
+class BMI {
+    //Constructor used to initialise data in class
+    constructor(h, w) {
+        this.height = h;
+        this.weight = w;
+    }
+
+    //Method to calculate BMI
+    calculateBMI() {
+        let bmi = this.weight / (this.height ** 2);
+        return bmi;
+    }
+}
+
+//Create an instance of class BMI
+let myBMI = new BMI(4, 50);
+
+//Print to screen
+console.log(myBMI.calculateBMI());
+
+var height, weight;
+
+class BMI{
+
+    constructor(h, w){
+        this.height = h;
+        this.weight = w;
+    }
+
+    calculateBMI(){
+        let bmi = this.weight/(this.height**2);
+        console.log(bmi);
+    }
+}
+
+p1 = new BMI(4,5);
+p1.calculateBMI();
